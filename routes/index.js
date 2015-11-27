c34863ee2dfc1fdd23b7021649c9e2d58e201081
Jian-Huan Hong <jianhuan.hung@gmail.com>
2015-11-26 18:36:54 -0800

    AngularJS Hello World

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.sendFile('index.html', {
+        root: __dirname + '/../client/src/'
+    }); // load the single view file (angular will handle the page changes on the front-end)
 });
 
 module.exports = router;
