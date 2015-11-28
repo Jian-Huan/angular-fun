65a0c186c9776fdb64298ccfb94838aec44b4f76
Jian-Huan Hong <jianhuan.hung@gmail.com>
2015-11-28 12:36:45 -0800

    Use angular-ui-router.

--- client/src/home/home.js
+++ client/src/home/home.js
@@ -1,2 +1,15 @@
+angular
+    .module('home', [])
+    .config(function($stateProvider) {
+        $stateProvider
+            .state('home',{
+                url:'/',
+                templateUrl:'/src/home/home.tpl.html',
+                controller:'HomeController'
+        })
+    })
+    .controller('HomeController', function ( $scope ) {
+        // ...
+    });
 
 
